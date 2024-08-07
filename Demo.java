9d9f241a516cadef6d9d4d6498e3937f3ef7fc5f
anjali5s <tandukaranjali5@gmail.com>
2024-08-07 07:36:32 +0545

    demo

--- Demo.java
+++ Demo.java
@@ -2,7 +2,9 @@ class Demo
 {
 public static void main(String []args)
 {
+    int s=0;
 for(int i=0;i<args.length;i++)
-System.out.println(args[i]);
+s=s+Integer.parseInt(args[i]);
+System.out.println(s);
 }
 }
