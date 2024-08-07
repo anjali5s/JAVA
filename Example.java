d91ec82791fc850a1ec32da859f976b196a17757
Anjali Tandukar <tandukaranjali5@gmail.com>
2024-08-07 07:20:39 +0545

    first commit

---
+++ Example.java
@@ -0,0 +1,13 @@
+import java.util.*;
+ public class Example
+{
+public static void main (String args[])
+{
+System.out.print("Enter values:");
+Scanner sc=new Scanner(System.in);
+int a = sc.nextInt();
+int b = sc.nextInt();
+int c = a+b;
+System.out.println("sum="+c);
+}
+}
